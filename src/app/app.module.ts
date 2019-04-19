68e64a750f4c24c8de73b9c82c88e282ebfea747
Nicholas Law <nick@nickwlaw.com>
2019-04-18 21:12:40 -0400

    user-list successfully pulling list of users from db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { WorkExperienceComponent } from './work-experience/work-experience.compo
 import { EducationComponent } from './education/education.component';
 import { FormsModule } from '@angular/forms';
 import { UserListComponent } from './feature/user/user-list/user-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './service/user.service';
 
 @NgModule({
   declarations: [
@@ -18,9 +20,12 @@ import { UserListComponent } from './feature/user/user-list/user-list.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/feature/user/user-list/user-list.component.ts
+++ src/app/feature/user/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../service/user.service';
+import { User } from '../../../model/User.class';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+
+  constructor(private userSvc: UserService) { }
 
   ngOnInit() {
+    this.userSvc.list().subscribe(userSvcList => { this.users = userSvcList; console.log(this.users); });
   }
 
 }
